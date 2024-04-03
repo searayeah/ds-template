2171aadd8c005865ebe2164869aa62535d2166b1
searayeah <insslava@gmail.com>
2024-04-04 02:50:29 +0300

    Updated hooks

--- create_project.sh
+++ create_project.sh
@@ -4,8 +4,8 @@ set -e
 
 project_name=$1
 if [[ -z $project_name ]]; then
-	echo "Need to pass project name"
-	exit 1
+  echo "Need to pass project name"
+  exit 1
 fi
 
 project_folder_name="${project_name//_/-}"
@@ -16,8 +16,13 @@ work_dir=$(mktemp -d -p .)
 git clone https://github.com/searayeah/ds-template.git "$work_dir"
 mv "$work_dir" "$project_folder_name"
 mv "$project_folder_name/ds_template" "$project_folder_name/$project_python_name"
-sed -i -e "/name.*=/s/= .*/= \"$project_folder_name\"/1" "$project_folder_name/pyproject.toml"
+sed -i -e "/name.*=/s/= .*/= \"$project_folder_name\"/1" \
+  "$project_folder_name/pyproject.toml"
 
-cd "$project_folder_name" && rm -rf .git && git init --initial-branch="main" && git add -A && git commit -m "Initialized using ds-template"
+cd "$project_folder_name" &&
+  rm -rf .git &&
+  git init --initial-branch="main" &&
+  git add -A &&
+  git commit -m "Initialized using ds-template"
 
 echo "Created $project_name"
