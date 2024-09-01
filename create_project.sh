7ee1a63d75824fdd5b122efb5dccc17fa7169dae
searayeah <insslava@gmail.com>
2024-09-01 23:25:50 +0300

    Fix ipynb

--- create_project.sh
+++ create_project.sh
@@ -2,23 +2,6 @@
 
 set -e
 
-project_name=$1
-if [[ -z $project_name ]]; then
-  echo "Need to pass project name"
-  exit 1
-fi
-
-project_folder_name="${project_name//_/-}"
-project_python_name="${project_name//-/_}"
-
-work_dir=$(mktemp -d -p .)
-
-git clone https://github.com/searayeah/ds-template.git "$work_dir"
-mv "$work_dir" "$project_folder_name"
-mv "$project_folder_name/ds_template" "$project_folder_name/$project_python_name"
-sed -i -e "/name.*=/s/= .*/= \"$project_folder_name\"/1" \
-  "$project_folder_name/pyproject.toml"
-
 cd "$project_folder_name" &&
   rm -rf .git &&
   git init --initial-branch="main" &&
@@ -27,4 +10,21 @@ cd "$project_folder_name" &&
   rm create_project.sh &&
   rm init_project.sh
 
+project_name=$1
+if [[ -z $project_name ]]; then
+  echo "Need to pass project name"
+  exit 1
+fi
+
+git clone https://github.com/searayeah/ds-template.git "$work_dir"
+mv "$work_dir" "$project_folder_name"
+mv "$project_folder_name/ds_template" "$project_folder_name/$project_python_name"
+sed -i -e "/name.*=/s/= .*/= \"$project_folder_name\"/1" \
+  "$project_folder_name/pyproject.toml"
+
 echo "Created $project_name"
+
+project_folder_name="${project_name//_/-}"
+project_python_name="${project_name//-/_}"
+
+work_dir=$(mktemp -d -p .)
