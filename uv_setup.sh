c67eb8c12edfaa4d51088735ab7cc98a7a7a215b
searayeah <insslava@gmail.com>
2024-10-02 02:37:27 +0300

    checkpoint3

--- uv_setup.sh
+++ uv_setup.sh
@@ -3,7 +3,7 @@
 # Prompt for project name (optional)
 read -r -p "Enter the project name (leave blank - default to parent folder name): " project_folder_name
 
-if [[ -z "$project_folder_name" ]]; then
+if [[ -z $project_folder_name ]]; then
   project_folder_name=$(basename "$PWD")
 fi
 
@@ -33,7 +33,7 @@ echo "Readme choice: $readme_choice"
 # Prompt for Python version (optional)
 read -r -p "Enter the Python version to use (leave blank if 3.9): " python_version
 
-if [[ -z "$python_version" ]]; then
+if [[ -z $python_version ]]; then
   python_version="3.9"
 fi
 
@@ -46,22 +46,22 @@ echo "Python version without dot: $python_version_without_dot"
 cmd="uv init"
 
 # Add project name to the command if provided
-if [[ -n "$project_folder_name" ]]; then
+if [[ -n $project_folder_name ]]; then
   cmd="$cmd --name \"$project_folder_name\""
 fi
 
 # Add VCS option if provided
-if [[ -n "$vcs_choice" ]]; then
+if [[ -n $vcs_choice ]]; then
   cmd="$cmd --vcs \"$vcs_choice\""
 fi
 
 # Add --no-readme if the user chooses no
-if [[ "$readme_choice" == "no" ]]; then
+if [[ $readme_choice == "no" ]]; then
   cmd="$cmd --no-readme"
 fi
 
 # Add Python version if provided
-if [[ -n "$python_version" ]]; then
+if [[ -n $python_version ]]; then
   cmd="$cmd --python \"$python_version\""
 fi
 
@@ -89,26 +89,26 @@ git clone https://github.com/searayeah/ds-template.git
 
 cp ds-template/.editorconfig .
 
-if [[ "$vcs_choice" == "git" && -f ".gitignore" ]]; then
+if [[ $vcs_choice == "git" && -f ".gitignore" ]]; then
   echo "Removing default .gitignore"
   rm ".gitignore"
   cp ds-template/.gitignore .
 fi
 
-if [[ "$vcs_choice" == "no" && -f ".gitignore" ]]; then
+if [[ $vcs_choice == "no" && -f ".gitignore" ]]; then
   echo "Concatting current .gitignore and ds-template .gitignore"
   cat ds-template/.gitignore >>.gitignore
 fi
 
 read -r -p "Github actions (Y or n): " github_actions
 
-if [[ "$github_actions" == "Y" || "$github_actions" == "y" ]]; then
+if [[ $github_actions == "Y" || $github_actions == "y" ]]; then
   cp -r ds-template/.github .
 fi
 
 if [[ -d ".git" && ! -d ".dvc" ]]; then
   read -r -p "Initialize dvc (Y or n): " dvc_init
-  if [[ "$dvc_init" == "Y" || "$dvc_init" == "y" ]]; then
+  if [[ $dvc_init == "Y" || $dvc_init == "y" ]]; then
     dvc init
     cat ds-template/.dvc/config >>.dvc/config
   fi
@@ -138,24 +138,24 @@ mapfile -t hooks < <(grep -oP '(?<=- repo: https://github.com/)\S+' .pre-commit-
 for hook in "${hooks[@]}"; do
   read -r -p "Do you want to keep $hook? (Y or n): " choice
   choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
-  if [[ "$choice" == "n" || "$choice" == "no" ]]; then
+  if [[ $choice == "n" || $choice == "no" ]]; then
     remove_hook "$hook"
   fi
 
-  if [[ ("$choice" == "y" || "$choice" == "yes") && $hook == "adrienverge/yamllint.git" ]]; then
+  if [[ ($choice == "y" || $choice == "yes") && $hook == "adrienverge/yamllint.git" ]]; then
     echo "Copying .yamllint.yaml"
     cp ds-template/.yamllint.yaml .
   fi
 
-  if [[ ("$choice" == "y" || "$choice" == "yes") && $hook == "igorshubovych/markdownlint-cli" ]]; then
+  if [[ ($choice == "y" || $choice == "yes") && $hook == "igorshubovych/markdownlint-cli" ]]; then
     echo "Copying .markdownlint.yaml"
     cp ds-template/.markdownlint.yaml .
   fi
 
-  if [[ ("$choice" == "y" || "$choice" == "yes") && $hook == "nbQA-dev/nbQA" ]]; then
+  if [[ ($choice == "y" || $choice == "yes") && $hook == "nbQA-dev/nbQA" ]]; then
     read -r -p "Do you want to keep nbqa/flake8? (Y or n): " nbqa_choice
     nbqa_choice=$(echo "$nbqa_choice" | tr '[:upper:]' '[:lower:]')
-    if [[ "$nbqa_choice" == "n" || "$nbqa_choice" == "no" ]]; then
+    if [[ $nbqa_choice == "n" || $nbqa_choice == "no" ]]; then
       echo "Removing flake8 from nbqa"
       sed -i '/^\s*- id: nbqa-flake8/,/^$/d' .pre-commit-config.yaml
     fi
@@ -170,6 +170,7 @@ sed -i "s/--py[0-9]\+/--py${python_version_without_dot}/g" .pre-commit-config.ya
 uv run pre-commit install
 
 echo "Copying linters and formatters configuration"
+echo >>pyproject.toml
 awk '/\[tool.black\]/ {found=1} found' ds-template/pyproject.toml >>pyproject.toml
 
 echo "Setting up right python version in formatter and linters configuration"
