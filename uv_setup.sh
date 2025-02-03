1e92dab29390872c9e70bb8aa65fc777f1d3e625
searayeah <insslava@gmail.com>
2025-02-03 03:23:43 +0300

    Some updates

--- uv_setup.sh
+++ uv_setup.sh
@@ -81,7 +81,7 @@ if [[ -f "hello.py" ]]; then
   rm "hello.py"
 fi
 
-uv add pre-commit isort black flake8 flake8-bugbear flake8-simplify flake8-pyproject ruff mypy pylint deptry "nbqa[toolchain]" --dev --raw-sources
+uv add pre-commit isort "bandit[toml]" black flake8 flake8-bugbear flake8-simplify flake8-pyproject ruff mypy pylint deptry "nbqa[toolchain]" --dev --raw-sources
 
 uv add typing-extensions pandas-stubs types-pillow types-beautifulsoup4 types-tqdm types-seaborn types-requests types-pyyaml types-regex types-openpyxl types-pygments types-colorama types-decorator types-jsonschema types-protobuf types-psutil types-setuptools types-six types-tabulate --dev --raw-sources
 
